205782b7a830b4b866e24a784824fe892f090f49
H-hidetaka <sansanhidetaka@gmail.com>
2022-01-09 14:39:56 +0900

    [Modify]パスワードを隠す

---
+++ front/pages/login/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container
+    >
+    <v-row
+      justify="center"
+      align-content="center"
+
+    >
+      <v-col
+        cols=12
+        sm=10
+        md=10
+        lg=10
+        xl=7
+        >
+  <v-card
+    max-width="1200"
+    shaped
+    color="#FDFDF0"
+  >
+    <v-card-title>
+      <h3 class="display-1">ログイン</h3>
+    </v-card-title>
+        <v-text-field prepend-icon="mdi-email" label="メールアドレス" />
+      <v-form>
+        <v-text-field prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
+      </v-form>
+    <v-container>
+        <v-btn
+            text
+            class="lime lighten-4"
+            width="200"
+            color="black accent-1"
+            @click="next"
+            to="../login"
+          >ログイン
+        </v-btn>
+      </v-container>
+    <!-- </v-card-actions> -->
+  </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    data: () => {
+        return {
+          items: [
+          {
+          color: '#FFFFFF',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+          to: '/register/index'
+          },
+          {
+          color: '#952175',
+          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+          title: 'Halcyon Days',
+          artist: 'Ellie Goulding',
+          },
+          {
+          color: '#952175',
+          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+          title: 'Halcyon Days',
+          artist: 'Ellie Goulding',
+          },
+      ],
+        };
+    },
+});
+</script>
+
+<style scoped>
+
+</style>
--- front/pages/register/index.vue
+++ front/pages/register/index.vue
@@ -1,36 +1,54 @@
 <template>
-  <v-card
-    max-width="1200"
-    outlined
-    shaped
-    color="#FDFDF0"
-  >
-    <v-card-title>
-      <h3 class="display-1">登録情報入力</h3>
-    </v-card-title>
-        <v-text-field prepend-icon="mdi-email" label="メールアドレス" />
-      <v-form>
-        <v-text-field prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
-      </v-form>
-        <v-text-field prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード再入力" />
-      <v-text-field prepend-icon="mdi-account-circle"  label="ユーザー名" />
-      <v-text-field prepend-icon="mdi-account-circle"  label="フルネーム" />
-      <v-text-field prepend-icon="mdi-lock"  label="フルネーム＜カタカナ＞" />
-    <v-card-actions
+  <v-container
     >
-    <v-container>
-        <v-btn
-            text
-            class="lime lighten-4"
-            width="200"
-            color="black accent-1"
-            @click="next"
-            to="../login"
-          >会員登録
-        </v-btn>
-      </v-container>
-    </v-card-actions>
-  </v-card>
+    <v-row
+      justify="center"
+      align-content="center"
+
+    >
+      <v-col
+        cols=12
+        sm=10
+        md=10
+        lg=10
+        xl=7
+        >
+        <v-card
+          max-width="1200px"
+          shaped
+          color="#FDFDF0"
+        >
+          <v-card-title>
+            <h3 class="display-1">登録情報入力</h3>
+          </v-card-title>
+              <v-text-field prepend-icon="mdi-email" label="メールアドレス" />
+            <v-form>
+              <v-text-field
+              v-bind:type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
+            </v-form>
+              <v-text-field prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード再入力" />
+            <v-text-field prepend-icon="mdi-account-circle"  label="ユーザー名" />
+            <v-text-field prepend-icon="mdi-account-circle"  label="フルネーム" />
+            <v-text-field prepend-icon="mdi-lock"  label="フルネーム＜カタカナ＞" />
+          <v-card-actions
+          >
+          <v-container>
+              <v-btn
+                  text
+                  class="lime lighten-4"
+                  width="200"
+                  color="black accent-1"
+                  @click="next"
+                  to="../login"
+                >会員登録
+              </v-btn>
+            </v-container>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -39,11 +57,7 @@ import Vue from 'vue';
 export default Vue.extend({
     data: () => {
         return {
-          alignments: [
-            'start',
-            'center',
-            'end',
-          ],
+          showPassword: false ,
           items: [
           {
           color: '#FFFFFF',
