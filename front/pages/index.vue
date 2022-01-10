8f78994cab6993bf1a7bc258e947ce27ceb58d77
H-hidetaka <sansanhidetaka@gmail.com>
2022-01-10 10:45:45 +0900

    [Add]トップページをレスポンシブ対応に修正

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,85 +1,101 @@
 <template>
-  <v-card
-    max-width="auto"
-    class="mx-auto"
-  >
-    <v-system-bar
-      color="orange darken-2"
-      dark
-      height="60"
-    >
-      <v-spacer></v-spacer>
-      <v-btn
-        width="800"
-        light
-        block
+  <v-container
       >
-      <!-- 検索用フォーム機能を作成 -->
-      <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-    </v-system-bar>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-      </v-btn>
-    <v-container>
-      <v-row dense>
+      <v-row
+        justify="center"
+        align-content="center"
+      >
         <v-col
-          v-for="(item, i) in items"
-          :key="i"
-          cols="12"
-        >
-          <v-card
-            height="200"
-            :color="item.color"
-            white
+          cols=12
+          sm=10
+          md=10
+          lg=10
+          xl=7
           >
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title
-                  class="text-h5"
-                  v-text="item.title"
-                ></v-card-title>
-
-                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-
-                <v-card-actions>
-                  <v-btn
-                    v-if="item.artist === 'Ellie Goulding'"
-                    class="ml-2 mt-3"
-                    fab
-                    icon
-                    height="40px"
-                    right
-                    width="40px"
-                  >
-                    <v-icon>mdi-play</v-icon>
-                  </v-btn>
-
-                  <v-btn
-                    v-else
-                    class="ml-2 mt-5"
-                    outlined
-                    rounded
-                    small
-                  >
-                    START RADIO
-                  </v-btn>
-                </v-card-actions>
-              </div>
-
-              <v-avatar
-                class="ma-3"
-                size="125"
-                tile
+        <v-card
+          max-width="auto"
+          class="mx-auto"
+        >
+          <v-system-bar
+            color="orange darken-2"
+            dark
+            height="60"
+          >
+            <v-spacer></v-spacer>
+            <v-btn
+              width="800"
+              light
+              block
+            >
+            <!-- 検索用フォーム機能を作成 -->
+            <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </v-system-bar>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+            </v-btn>
+          <v-container>
+            <v-row dense>
+              <v-col
+                v-for="(item, i) in items"
+                :key="i"
+                cols="12"
               >
-                <v-img :src="item.src"></v-img>
-              </v-avatar>
-            </div>
-          </v-card>
+                <v-card
+                  height="200"
+                  :color="item.color"
+                  white
+                >
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title
+                        class="text-h5"
+                        v-text="item.title"
+                      ></v-card-title>
+
+                      <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+                      <v-card-actions>
+                        <v-btn
+                          v-if="item.artist === 'Ellie Goulding'"
+                          class="ml-2 mt-3"
+                          fab
+                          icon
+                          height="40px"
+                          right
+                          width="40px"
+                        >
+                          <v-icon>mdi-play</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          v-else
+                          class="ml-2 mt-5"
+                          outlined
+                          rounded
+                          small
+                        >
+                          START RADIO
+                        </v-btn>
+                      </v-card-actions>
+                    </div>
+
+                    <v-avatar
+                      class="ma-3"
+                      size="125"
+                      tile
+                    >
+                      <v-img :src="item.src"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
         </v-col>
       </v-row>
-    </v-container>
-  </v-card>
+          </v-container>
+        </v-card>
+        </v-col>
+      </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
