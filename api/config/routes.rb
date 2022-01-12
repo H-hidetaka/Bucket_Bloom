a5f465be59d90dbf4445d936006b7eaa81b769fa
H-hidetaka <sansanhidetaka@gmail.com>
2022-01-12 17:48:13 +0900

    [Modify]コントローラー修正

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users_sessions/index'
+    get 'users_sessions/show'
+    get 'users_sessions/create'
+    get 'users_sessions/update'
+    get 'users_sessions/destroy'
+  end
   get 'user_sessions/new'
   get 'user_sessions/create'
   get 'user_sessions/destroy'
   root :to => 'users#index'
   resources :users
+  resources :user_sessions
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
