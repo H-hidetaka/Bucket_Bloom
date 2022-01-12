1a95e39bcd18ad336540c904b3dc6c810473e059
H-hidetaka <sansanhidetaka@gmail.com>
2022-01-12 14:47:41 +0900

    [Add]ユーザーDB追加、sorceryのgem追加

---
+++ api/db/migrate/20220112041652_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name,                  null: false
+      t.string :email,                 null: false
+      t.string :crypted_password
+      t.string :salt
+      t.string :avatar
+
+      t.timestamps                    null: false
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
