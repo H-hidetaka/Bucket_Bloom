0652f22b9a0d9d2410b1228b70c3c2ea27c9b47c
H-hidetaka <sansanhidetaka@gmail.com>
2022-04-29 14:49:57 +0900

    [Add]一時的な保存

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::API
   # skip_before_action :verify_authenticity_token, if: :devise_controller
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   def http_authenticate
     authenticate_or_request_with_http_digest do |user_name, password|
       user_name == "foo" && password == "bar"
@@ -13,5 +12,6 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name ,:encrypted_password , :email])
+    devise_parameter_sanitizer.permit(:session, keys: [:name ,:encrypted_password , :email])
   end
 end
